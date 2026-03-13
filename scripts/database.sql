f2f9174a5c795bb4c9a536027abe5a0e30bba2a7
Ike-Ikechukwu <ike.ikechukwu95@gmail.com>
2026-03-13 02:00:33 +0100

    Update database.sql

--- scripts/database.sql
+++ scripts/database.sql
@@ -1,1 +1,41 @@
+/*
+========================================================
+Create Database and Schemas
+========================================================
+Script Purpose:
+	This Script creates a new Database named 'beauty_spot_DW
+	along with three schemas named 'bronze', 'silver', and 
+	'gold' within the database afer checking if it exists.
 
+WARNING:
+	Running this script will drop the entire 'DataWarehouse'
+	database ie. All data in the database will be permantly
+	deleted. Please proceed with caution and ensure you have 
+	proper backups before running the script.
+*/
+
+USE master;
+
+-- Drop and recreate the 'DataWarehouse' database if it exists
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'beauty_spot_DW')
+BEGIN
+	DROP DATABASE beauty_spot_DW;
+END;
+GO
+
+--Create the 'beauty_spot_DW' database
+CREATE DATABASE beauty_spot_DW;
+GO
+
+USE beauty_spot_DW;
+GO
+
+--Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
